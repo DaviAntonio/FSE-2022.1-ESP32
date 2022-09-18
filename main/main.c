1f07ec8862acee177935804febf0aeba42d54fd2
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-09-17 23:58:04 -0300

    feat: read from DHT11 and publish data using MQTT

--- main/main.c
+++ main/main.c
@@ -7,12 +7,18 @@
 #include "esp_log.h"
 #include "freertos/semphr.h"
 
+#include "dht11.h"
+
 #include "wifi.h"
 #include "http_client.h"
 #include "mqtt.h"
 
 xSemaphoreHandle conexaoWifiSemaphore;
 xSemaphoreHandle conexaoMQTTSemaphore;
+
+int temperature = 0;
+int humidity = 0;
+bool valid_dht11 = false;
 
 void conectadoWifi(void * params)
 {
@@ -26,20 +32,64 @@ void conectadoWifi(void * params)
 
 void trataComunicacaoComServidor(void * params)
 {
-	char mensagem[50];
+	char mensagem[60];
+	bool too_hot;
+	bool too_cold;
+	bool too_dry;
+	bool too_wet;
+
 	if (xSemaphoreTake(conexaoMQTTSemaphore, portMAX_DELAY)) {
 		while (true) {
-			float temperatura = 20.0f + (float)rand()/(float)(RAND_MAX/10.0f);
-			int hot = temperatura > 25.0f;
+			if (valid_dht11) {
+				too_hot = temperature > 30;
+				too_cold = temperature < 20;
+				too_dry = humidity < 30;
+				too_wet = humidity > 70;
 
-			sprintf(mensagem, "{\"temperatura_fake\": %f}", temperatura);
-			mqtt_envia_mensagem("v1/devices/me/telemetry", mensagem);
+				sprintf(mensagem,
+					"{\"temperature\": %d, \"humidity\": %d}",
+					temperature, humidity);
+				mqtt_envia_mensagem("v1/devices/me/telemetry",
+					mensagem);
 
-			sprintf(mensagem, "{\"is_hot\": %d}", hot);
-			mqtt_envia_mensagem("v1/devices/me/attributes", mensagem);
+				sprintf(mensagem,
+					"{\"too_hot\": %d, \"too_cold\": %d, \"too_wet\": %d, \"too_dry\": %d}",
+					too_hot, too_cold, too_wet, too_dry);
+				mqtt_envia_mensagem("v1/devices/me/attributes",
+					mensagem);
+			}
 
 			vTaskDelay(3000 / portTICK_PERIOD_MS);
 		}
+	}
+}
+
+void readDHT11(void *params)
+{
+	while (true) {
+		struct dht11_reading dht11_data = DHT11_read();
+		if (dht11_data.status == DHT11_OK) {
+			temperature = dht11_data.temperature;
+			humidity = dht11_data.humidity;
+			valid_dht11 = true;
+			ESP_LOGI("DHT11", "Read successfully");
+		} else {
+			switch (dht11_data.status) {
+			case DHT11_CRC_ERROR:
+				ESP_LOGE("DHT11", "Read failed with status %d (CRC MISMATCH)", dht11_data.status);
+				break;
+			case DHT11_TIMEOUT_ERROR:
+				ESP_LOGE("DHT11", "Read failed with status %d (TIMEOUT)", dht11_data.status);
+				break;
+			default:
+				ESP_LOGE("DHT11", "Read failed with status %d", dht11_data.status);
+			}
+		}
+		if (valid_dht11) {
+			ESP_LOGI("DHT11", "Last temp: %d", temperature);
+			ESP_LOGI("DHT11", "Last hum: %d", humidity);
+		}
+		vTaskDelay(4000 / portTICK_PERIOD_MS);
 	}
 }
 
@@ -56,9 +106,14 @@ void app_main(void)
 
 	conexaoWifiSemaphore = xSemaphoreCreateBinary();
 	conexaoMQTTSemaphore = xSemaphoreCreateBinary();
+
+	DHT11_init(GPIO_NUM_16);
+	ESP_LOGI("DHT11", "Initialised\n");
+
 	wifi_start();
 
 	xTaskCreate(&conectadoWifi, "Conexão ao MQTT", 4096, NULL, 1, NULL);
 	xTaskCreate(&trataComunicacaoComServidor, "Comunicação com Broker",
 		4096, NULL, 1, NULL);
+	xTaskCreate(&readDHT11, "DHT11 reading", 4096, NULL, 1, NULL);
 }
