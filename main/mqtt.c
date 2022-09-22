92485ea87bcece7bbdc3e77a4282c757efa25768
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-09-22 00:55:51 -0300

    style(main/mqtt.c): limit to 80 columns

--- main/mqtt.c
+++ main/mqtt.c
@@ -245,10 +245,12 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
 
 		xSemaphoreGive(conexaoMQTTSemaphore);
 
-		msg_id = esp_mqtt_client_subscribe(client, "v1/devices/me/rpc/request/+", 0);
+		msg_id = esp_mqtt_client_subscribe(client,
+			"v1/devices/me/rpc/request/+", 0);
 		ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-		msg_id = esp_mqtt_client_subscribe(client, "v1/devices/me/attributes/response/+", 0);
+		msg_id = esp_mqtt_client_subscribe(client,
+			"v1/devices/me/attributes/response/+", 0);
 		ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 		break;
 	case MQTT_EVENT_DISCONNECTED:
